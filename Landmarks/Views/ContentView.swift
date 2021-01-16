4ba13bc6f0a05c52c558b5c25d0882ccc3a7ee85
James Boyer <jamesboyer92@user.noreply.github.com>
2021-01-16 17:25:58 -0500

    created a ListView to map out LandMarkrows, wrapped that list view in a navigationView and passed navigationLink into the Landmark rows so they can navigation into the landmark detail view. Landmark details. App now is reading data from a json file and static images placed within the assets

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by James Boyer on 1/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarksList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/Views/MapView.swift
@@ -0,0 +1,36 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by James Boyer on 1/16/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+    
+    @State private var region = MKCoordinateRegion()
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
+            }
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        )
+    }
+}
+
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
+    }
+}
